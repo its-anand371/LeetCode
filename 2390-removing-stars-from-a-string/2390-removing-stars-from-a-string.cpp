7abd55d45a1623c78839132bd30608b9b94649e9
Anand singh shekhawat <anand1.btmtcs25@dl.nfsu.edu.in>
2026-07-24 12:27:50 +0530

    Time: 11 ms (89.42%), Space: 30 MB (10.93%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char>st;
+        for(char &c:s){
+            if(c=='*'){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        string result="";
+        while(!st.empty()){
+            result+=st.top();
+            st.pop();
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
