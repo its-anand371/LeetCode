f88a004364d37b053215234dbb79d73c99a3fc80
Anand singh shekhawat <anand1.btmtcs25@dl.nfsu.edu.in>
2026-08-09 17:36:55 +0530

    Time: 15 ms (92.45%), Space: 18.7 MB (10.11%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,48 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> result;
-
-    void two_sum(vector<int>& nums, int a, int b,
-                 int start, int end, long long remainder) {
-
-        while (start < end) {
-            long long sum = 1LL * nums[start] + nums[end];
-
-            if (sum > remainder) {
-                end--;
+    
+    void two_sum(vector<int>& nums, long long target, int a, int b, int start, int end){
+        while(start < end){
+            long long sum = nums[start] + nums[end];
+            if(sum < target){
+                start += 1;
             }
-            else if (sum < remainder) {
-                start++;
+            else if(sum > target){
+                end -= 1;
             }
-            else {
+            else{
                 result.push_back({a, b, nums[start], nums[end]});
-
-                while (start < end && nums[start] == nums[start + 1]) start++;
-                while (start < end && nums[end] == nums[end - 1]) end--;
-
-                start++;
-                end--;
+                start += 1;
+                end -= 1;
+                while(start < end && nums[start] == nums[start-1]) start += 1;
+                while(start < end && nums[end] == nums[end+1]) end -= 1;
             }
         }
     }
-
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                long long remainder = 1LL * target - nums[i] - nums[j];
-
-                two_sum(nums, nums[i], nums[j],
-                        j + 1, n - 1, remainder);
+        result.clear();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n - 3; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j < n - 2; j++){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                long long remainder = (long long)target - nums[i] - nums[j];
+                two_sum(nums, remainder, nums[i], nums[j], j+1, n-1);
             }
         }
-
         return result;
     }
 };
