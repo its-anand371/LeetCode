528708aadca4739d88363cf5f3789a2cba28c2d8
Anand singh shekhawat <anand1.btmtcs25@dl.nfsu.edu.in>
2026-08-09 18:38:36 +0530

    Time: 0 ms (100%), Space: 10.3 MB (38.18%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m =matrix.size();
+        int n =matrix[0].size();
+        for(int i =0;i<m;i++){
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i =0;i<m;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right){
+                swap(matrix[i][left],matrix[i][right]);
+                left+=1;
+                right-=1;
+            }
+        }
+    }
+};
