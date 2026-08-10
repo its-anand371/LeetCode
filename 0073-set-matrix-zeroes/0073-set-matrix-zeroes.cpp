e8628e9410a2b8f699bfc11e929dbe35de3f822d
Anand singh shekhawat <anand1.btmtcs25@dl.nfsu.edu.in>
2026-08-10 16:58:47 +0530

    Time: 0 ms (100%), Space: 20.9 MB (57.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,41 +5,41 @@ public:
         int n =matrix[0].size();
         bool firstrowimpacted=false;
         bool firstcolimpacted=false;
-        for(int col=0;col<n;col++){
-            if(matrix[0][col]==0){
-                firstrowimpacted=true;
-                break;
-            }
-        }
-        for(int row=0;row<m;row++){
-            if(matrix[row][0]==0){
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0){
                 firstcolimpacted=true;
                 break;
             }
         }
-        for(int i =1;i<m;i++){
-            for(int j =1;j<n;j++){
+        for(int i=0;i<n;i++){
+            if(matrix[0][i]==0){
+                firstrowimpacted=true;
+                break;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
                     matrix[i][0]=0;
                     matrix[0][j]=0;
                 }
             }
         }
-        for(int i =1;i<m;i++){
-            for(int j =1;j<n;j++){
-                if(matrix[i][0]==0||matrix[0][j]==0){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        if(firstrowimpacted){
-            for(int j=0;j<n;j++){
-                matrix[0][j]=0;
+        if (firstrowimpacted) {
+            for (int k = 0; k < n; k++) {
+                matrix[0][k] = 0;  
             }
         }
-        if(firstcolimpacted){
-            for(int i=0;i<m;i++){
-                matrix[i][0]=0;
+        if (firstcolimpacted) {
+            for (int k = 0; k < m; k++) {
+                matrix[k][0] = 0;  
             }
         }
     }
