cda9da1922f4870fb5a398d44bc659789788ae8d
Anand singh shekhawat <anand1.btmtcs25@dl.nfsu.edu.in>
2026-08-10 11:43:23 +0530

    Time: 0 ms (100%), Space: 72.6 MB (96.06%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<int> replaceElements(vector<int>& arr) {
-        int n =arr.size();
+    vector<int> replaceElements(vector<int>& nums) {
+        int n=nums.size();
         int maxsofar=-1;
         for(int i =n-1;i>=0;i--){
-            int curr=arr[i];
-            arr[i]=maxsofar;
+            int curr=nums[i];
+            nums[i]=maxsofar;
             maxsofar=max(maxsofar,curr);
-        }   
-        return arr;
+        }
+        return nums;
     }
 };
