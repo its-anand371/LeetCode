e1379c8fac1867d86fa2e384b6981a267638681c
Anand singh shekhawat <anand1.btmtcs25@dl.nfsu.edu.in>
2026-08-10 10:54:27 +0530

    Time: 0 ms (100%), Space: 71.9 MB (15.18%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int sum=0;
+        int maximum=INT_MIN;
+        for(int i =0;i<n;i++){
+            sum+=nums[i];
+            maximum=max(sum, maximum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maximum;
+    }
+};
