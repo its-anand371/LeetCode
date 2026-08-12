b6798a735158cc032d956b7f754494cf54e524fa
Anand singh shekhawat <anand1.btmtcs25@dl.nfsu.edu.in>
2026-08-12 16:46:16 +0530

    Time: 27 ms (27.76%), Space: 70.6 MB (48.7%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-    int minimum(vector<int>&nums,int n ){
-        int minimum=INT_MAX;
-        for(int i =0;i<n;i++){
-            minimum=min(minimum,nums[i]);
-        }
-        return minimum;
-    }
-    int maximum(vector<int>&nums,int n ){
-        int maximum=INT_MIN;
-        for(int i =0;i<n;i++){
-            maximum=max(maximum,nums[i]);
-        }
-        return maximum;
-    }
-    int bloom(vector<int>&nums,int &k ,int &mid){
+    int days(vector<int>&nums,int &limit,int k){
         int bouq_count=0;
-        int cons_count=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<=mid){
-                cons_count++;
+        int days_count=0;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]<=limit){
+                days_count+=1;
             }
             else{
-                cons_count=0;
+                days_count=0;
             }
-            if(cons_count==k){
+            if(days_count==k){
                 bouq_count+=1;
-                cons_count=0;
+                days_count=0;
             }
         }
         return bouq_count;
     }
-    int minDays(vector<int>& nums, int m, int k) {
-        int n = nums.size();
-        int start=minimum(nums,n);
-        int end=maximum(nums,n);
-        if((long long )m*n<nums.size()){
-            return -1;
-        }
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n =bloomDay.size();
+        int start=1;
+        int end=*max_element(bloomDay.begin(),bloomDay.end());
         int ans=-1;
+        int l=0;
         while(start<=end){
-            int mid=start+(end-start)/2;
-            int n=bloom(nums,k,mid);
-            if(n>=m){
+            int mid=start+((end-start)/2);
+            l = days(bloomDay,mid,k);
+            if(l>=m){
                 ans=mid;
                 end=mid-1;
             }
